c40f17ef2a8daa906138daa251a6006fd635b1b4
HuseObeid <56361444+HuseObeid@users.noreply.github.com>
2020-12-13 16:45:39 -0500

    [MemeVC] Adding memeview, keyboard and labels (#3)

---
+++ hus-demo/hus-demo/Models/MemeModel.swift
@@ -0,0 +1,39 @@
+//
+//  MemeModel.swift
+//  hus-demo
+//
+//  Created by Hussein Obeid on 2020-09-21.
+//  Copyright © 2020 Adrian Domanico. All rights reserved.
+//
+
+import UIKit
+
+class MemeModel: NSObject {
+    var image: UIImage?
+    var topText: String?
+    var bottomText: String?
+    var memedImage: UIImageView?
+    
+    struct Meme {
+        var topTextField: String
+        var bottomTextFiled: String
+        var firstImage: UIImage
+        var memedImage: UIImage
+    }
+    
+    init(topText: String?, bottomText: String?, image: UIImage?) {
+        super.init()
+        self.topText = topText
+        self.bottomText = bottomText
+        self.image = image
+    }
+}
+
+
+
+
+
+
+
+
+
